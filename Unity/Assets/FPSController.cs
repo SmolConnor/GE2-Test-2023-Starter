913fc302411945129d6d2f5539a39ea297065852
SmolConnor <77941492+SmolConnor@users.noreply.github.com>
2023-04-25 10:12:43 +0100

    if the camera collides pod inPod becomes true
    touching off the pod stops movement

--- Unity/Assets/FPSController.cs
+++ Unity/Assets/FPSController.cs
@@ -24,6 +24,14 @@ public class FPSController : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Bullet"))
+        {
+            inPod = true;
+        }
+    }
+
     void Yaw(float angle)
     {
         Quaternion rot = Quaternion.AngleAxis(angle, Vector3.up);
